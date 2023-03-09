d5b8cc5ade847cce70498ac9cdb1b07c0ab93573
AyteD <carlblandin@gmail.com>
2023-03-09 10:57:27 +0100

    LV_3 first commit
    il manque les triggers

--- 2D_Initiation_1JV/Assets/Scripts/EndingLevel.cs
+++ 2D_Initiation_1JV/Assets/Scripts/EndingLevel.cs
@@ -8,23 +8,46 @@ public class EndingLevel : MonoBehaviour
     [SerializeField]
     public string levelname;
 
+    int playersintrigger = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter2D(Collider2D pCollision)
     {
-        //if (pCollision.gameObject.CompareTag("Player"))
-        //{
-            SceneManager.LoadScene(levelname);
-        //}
+        if (pCollision.gameObject.CompareTag("Player"))
+        {
+
+            playersintrigger++;
+
+        }
+
+        if (playersintrigger >= 2)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D pCollision)
+    {
+        if (pCollision.gameObject.CompareTag("Player"))
+        {
+            playersintrigger--;
+
+        }
+
+
     }
 }
